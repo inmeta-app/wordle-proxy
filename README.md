2f6db51ce5b712fdc086961f64cad7100ca1d61f
Pol Vallverdu <86187892+polvallverdu@users.noreply.github.com>
2024-08-22 11:26:22 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Wordle Proxy
+
+A proxy for requesting wordle words. Will return the current one.
+
+## Included Wordles
+
+- [New York Times](https://www.nytimes.com/games/wordle/index.html)
+
+### Coming Soon
+
+- [La Palabra del día](https://lapalabradeldia.com/)
+
+## Deploy
+
+Deploy with bun or cloudflare workers.
+
+### Bun
+
+```bash
+bun i
+bun dev
+```
+
+### Cloudflare Workers
+
+```bash
+bun i
+wrangler dev
+```
---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import { Hono } from "hono";
+import { cache } from "hono/cache";
+import { cors } from "hono/cors";
+
+const app = new Hono();
+
+function formatTimestamp(timestamp: number) {
+  const date = new Date(timestamp);
+  const year = date.getFullYear();
+  const day = String(date.getDate()).padStart(2, "0");
+  const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are 0-indexed
+
+  return `${year}-${month}-${day}`;
+}
+
+app.use(
+  "/",
+  cors({
+    origin: ["https://wordle.inmeta.site", "https://inmeta.app"],
+    allowMethods: ["GET", "OPTIONS"],
+    exposeHeaders: ["Content-Length"],
+  })
+);
+
+app.get(
+  "/",
+  cache({
+    cacheName: "wordle",
+    cacheControl: "max-age=600",
+  })
+);
+
+app.get("/", async (c) => {
+  const wordleUrl = `https://www.nytimes.com/svc/wordle/v2/${formatTimestamp(
+    Date.now()
+  )}.json`;
+
+  try {
+    const response = await fetch(wordleUrl);
+    const data: {
+      id: number;
+      solution: string;
+      print_date: string;
+      days_since_launch: number;
+      editor: string;
+    } = await response.json();
+    return c.json(data);
+  } catch (error) {
+    c.status(500);
+    return c.json({ error: "Internal Server Error" });
+  }
+});
+
+export default app;
